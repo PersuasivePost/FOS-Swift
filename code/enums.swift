f601e2a9ff1cc01f0d31e9746b9d396673ea20f0
Ashvatth Joshi <codingash27@gmail.com>
2025-05-17 09:54:04 +0530

    rrfu

---
+++ code/enums.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+// Enums is the same as Struct except we know all cases at runtime
+
+struct carModel {
+    let brand: String
+    let model: String
+}
+
+struct CarBrand {
+    let title: String
+}
+
+// var car1: carModel = carModel(brand: "Ford", model: "Mustang")
+// var car2: carModel = carModel(brand: "Ford", model: "Alto")
+// var car3: carModel = carModel(brand: "Mahindra", model: "XUV700")
+
+var car1: CarModel 
